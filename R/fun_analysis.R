fd22eff4a855286c923c6cc5cad1fe962f354186
ikegami <maikegam@ncc.go.jp>
2020-11-14 22:03:44 +0900

    0/0 error corrected

--- R/fun_analysis.R
+++ R/fun_analysis.R
@@ -111,7 +111,8 @@ fun_analysis = function(MSEC,
            MSEC$Altered_length + 2 - 
            MSEC$minimum_length_2) - 
     mapply(function(x, y) {return (Mut_depth[x,y])}, 
-           1:dim(MSEC)[1], MSEC$minimum_length_1 + 1)
+           1:dim(MSEC)[1], 
+           MSEC$minimum_length_1 + 1)
   MSEC = MSEC %>% mutate(
     Short_short_support_sum = 
       (short_support_length_adjust_sum <= 
@@ -203,51 +204,51 @@ fun_analysis = function(MSEC,
                    "Filter 3: p is small, but supported enough long,"), 
              Caution)
   )
-MSEC = MSEC %>% mutate(
-    MSEC_filter_1234 = ifelse(
-      Filter_1_mutation_intra_hairpin_loop |
-      Filter_2_hairpin_structure | 
-      Filter_3_microhomology_induced_mutation | 
-      Filter_4_soft_clipping,
-      "Artifact suspicious", ""),
-    MSEC_filter_12345 = ifelse(
-      Filter_1_mutation_intra_hairpin_loop |
-      Filter_2_hairpin_structure | 
-      Filter_3_microhomology_induced_mutation | 
-      Filter_4_soft_clipping |
-      Filter_5_highly_homologous_region,
-      "Artifact suspicious", ""),
-    MSEC_filter_all = ifelse(
-      Filter_1_mutation_intra_hairpin_loop |
-      Filter_2_hairpin_structure | 
-      Filter_3_microhomology_induced_mutation | 
-      Filter_4_soft_clipping |
-      Filter_5_highly_homologous_region | 
-      Filter_6_simple_repeat |
-      Filter_7_C_to_T_artifact |
-      Filter_8_mutation_at_homopolymer,
-      "Artifact suspicious", ""),
-    Comment = Caution
-  )
-MSEC = MSEC %>% select(-mut_type, -Alt_length, -Hairpin_length, 
-        -Pre_Minimum_length, -Post_Minimum_length,
-        -Low_quality_base_rate_under_Q18, -Pre_rep_status, -Post_rep_status, 
-        -Homopolymer_status, -indel_status, -indel_length, -penalty_Pre,
-        -penalty_Post, -Caution, -Pre_Minimum_length_adjust, -Half_length,
-        -Post_Minimum_length_adjust, -Pre_support_length_adjust,
-        -Post_support_length_adjust, -shortest_support_length_adjust,
-        -minimum_length_1, -minimum_length_2, -minimum_length,
-        -short_support_length_adjust, -Altered_length, -distant_homology,
-        -short_support_length_total, -Pre_support_length_total,
-        -Post_support_length_total, -Half_length_total,
-        -Total_length_total, -Short_short_support, -Short_pre_support,
-        -Short_post_support, -High_rate_Q18, -Not_long_repeat,
-        -Short_short_support_sum, -Short_pre_support_sum,
-        -Short_post_support_sum, -short_support_length_adjust_sum,
-        -Pre_support_length_adjust_sum, -Post_support_length_adjust_sum,
-        -Half_length_adjust_sum, -Total_length_adjust_sum
-        )
-return(MSEC)
+  MSEC = MSEC %>% mutate(
+      MSEC_filter_1234 = ifelse(
+        Filter_1_mutation_intra_hairpin_loop |
+        Filter_2_hairpin_structure | 
+        Filter_3_microhomology_induced_mutation | 
+        Filter_4_soft_clipping,
+        "Artifact suspicious", ""),
+      MSEC_filter_12345 = ifelse(
+        Filter_1_mutation_intra_hairpin_loop |
+        Filter_2_hairpin_structure | 
+        Filter_3_microhomology_induced_mutation | 
+        Filter_4_soft_clipping |
+        Filter_5_highly_homologous_region,
+        "Artifact suspicious", ""),
+      MSEC_filter_all = ifelse(
+        Filter_1_mutation_intra_hairpin_loop |
+        Filter_2_hairpin_structure | 
+        Filter_3_microhomology_induced_mutation | 
+        Filter_4_soft_clipping |
+        Filter_5_highly_homologous_region | 
+        Filter_6_simple_repeat |
+        Filter_7_C_to_T_artifact |
+        Filter_8_mutation_at_homopolymer,
+        "Artifact suspicious", ""),
+      Comment = Caution
+    )
+  MSEC = MSEC %>% select(-mut_type, -Alt_length, -Hairpin_length, 
+          -Pre_Minimum_length, -Post_Minimum_length,
+          -Low_quality_base_rate_under_Q18, -Pre_rep_status, -Post_rep_status, 
+          -Homopolymer_status, -indel_status, -indel_length, -penalty_Pre,
+          -penalty_Post, -Caution, -Pre_Minimum_length_adjust, -Half_length,
+          -Post_Minimum_length_adjust, -Pre_support_length_adjust,
+          -Post_support_length_adjust, -shortest_support_length_adjust,
+          -minimum_length_1, -minimum_length_2, -minimum_length,
+          -short_support_length_adjust, -Altered_length, -distant_homology,
+          -short_support_length_total, -Pre_support_length_total,
+          -Post_support_length_total, -Half_length_total,
+          -Total_length_total, -Short_short_support, -Short_pre_support,
+          -Short_post_support, -High_rate_Q18, -Not_long_repeat,
+          -Short_short_support_sum, -Short_pre_support_sum,
+          -Short_post_support_sum, -short_support_length_adjust_sum,
+          -Pre_support_length_adjust_sum, -Post_support_length_adjust_sum,
+          -Half_length_adjust_sum, -Total_length_adjust_sum
+          )
+  return(MSEC)
 }
 
 # The following block is used by usethis to automatically manage
