6c3261161f290f9ac91ddb5e662843c68c6b5ea9
ikegami <maikegam@ncc.go.jp>
2020-11-25 15:47:53 +0900

    v1.1.2

--- R/fun_read_check.R
+++ R/fun_read_check.R
@@ -656,8 +656,10 @@ fun_read_check <- function(df_mutation,
           penalty_post <- max(0, 4 * alt_length - 5) + 5 * co_mut_post
         }
 
-        if (fun_zero(near_indel_pre, near_indel_pre_candidate) == 1 |
-            fun_zero(near_indel_post, near_indel_post_candidate) == 1) {
+        if ((fun_zero(near_indel_pre, near_indel_pre_candidate) == 1 &
+             pre_minimum_length >= 10)|
+            (fun_zero(near_indel_post, near_indel_post_candidate) == 1 &
+             post_minimum_length >= 10)) {
           pre_support_length <- min(pre_support_length + post_minimum_length,
                                     read_length)
           post_support_length <- min(post_support_length + pre_minimum_length,
