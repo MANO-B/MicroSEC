d05d8e7f8675efd532e08466543c042baa092bdb
ikegami <maikegam@ncc.go.jp>
2020-12-14 17:10:02 +0900

    v1.1.11

--- doc/my-vignette.R
+++ doc/my-vignette.R
@@ -7,7 +7,8 @@ knitr::opts_chunk$set(collapse = TRUE,
                       warning = FALSE,
                       message = TRUE,
                       comment = "#>")
-options(rmarkdown.html_vignette.check_title = FALSE)
+options(rmarkdown.html_vignette.check_title = FALSE, show.error.messages = FALSE, warn = -1)
+
 progress_bar = "N"
 
 ## ----packages-----------------------------------------------------------------
