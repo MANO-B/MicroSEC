85a3b877570800b66dfd6b5a340cbe4d875498f3
ikegami <maikegam@ncc.go.jp>
2021-04-05 22:07:40 +0900

    v1.2.4

--- R/fun_load_bam.R
+++ R/fun_load_bam.R
@@ -19,6 +19,8 @@ fun_load_bam <- function(bam_file) {
                       "seq", "qual", "pos")]
   df_bam$strand <<- as.character(df_bam$strand)
   df_bam$strand[which(is.na(df_bam$strand))] <<- "NA"
+  df_bam$pos[which(is.na(df_bam$pos))] <<- 0
+  df_bam$cigar[which(is.na(df_bam$cigar))] <<- "150H"
 }
 
 # The following block is used by usethis to automatically manage
