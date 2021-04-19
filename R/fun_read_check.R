51ca990691ae0233bbbcaedbadf76942ea14c610
Ikegami Masachika <ikegami@Ikegamis-MacBook-Air.local>
2021-04-19 19:08:19 +0900

    test

--- R/fun_read_check.R
+++ R/fun_read_check.R
@@ -411,7 +411,6 @@ fun_read_check <- function(short_homology_search_length) {
                     asc(as.character(df_bam_qual[[depth]])))
                   if (mean(cigar_qual) >= 53) {
                     for (k in seq_len(length(cigar_type))) {
-                      print(cigar_pos)
                       if (cigar_pos == mut_pos &
                           cigar_num[k] == alt &
                           cigar_type[k] == "D") {
