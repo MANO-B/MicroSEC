456a8a1c062d4ca4c7723d0a4f0eb1b759177aa2
MANO-B <maikegam@ncc.go.jp>
2024-06-16 15:15:56 +0900

    Update fun_load_bam.R

--- R/fun_load_bam.R
+++ R/fun_load_bam.R
@@ -20,14 +20,11 @@ fun_load_bam <- function(bam_file) {
   df_bam$strand <<- as.character(df_bam$strand)
   df_bam$strand[which(is.na(df_bam$strand))] <<- "NA"
   df_bam$pos[which(is.na(df_bam$pos))] <<- 0
-  df_bam$pos[which(is.na(df_bam$cigar))] <<- 0
-  df_bam$pos[which(df_bam$cigar == "*")] <<- 0
-  df_bam$pos[which(df_bam$cigar == "0*")] <<- 0
-  df_bam$pos[which(df_bam$cigar == "*=")] <<- 0
   df_bam$cigar[which(is.na(df_bam$cigar))] <<- "150H"
   df_bam$cigar[which(df_bam$cigar == "*")] <<- "150H"
   df_bam$cigar[which(df_bam$cigar == "0*")] <<- "150H"
   df_bam$cigar[which(df_bam$cigar == "*=")] <<- "150H"
+  df_bam$pos[which(df_bam$cigar == "150H")] <<- 0
 }
 
 # The following block is used by usethis to automatically manage
